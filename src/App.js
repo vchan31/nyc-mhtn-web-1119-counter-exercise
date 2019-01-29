96078f2cd85950d39c3da1473ab153777f5f61b6
Vincent Chan <vchan313@gmail.com>
2019-01-29 16:28:25 -0500

    did lab

--- src/App.js
+++ src/App.js
@@ -4,18 +4,44 @@ import "./App.css";
 
 class App extends Component {
   state = {
-    number: 0
+    number: 0    
   };
+
+  handleAdd = (e)=>{
+    this.setState({
+      number:this.state.number + 1
+    })
+  }//end of handleAdd
+
+  handleMinus = (e)=>{
+    this.setState({
+      number: this.state.number - 1
+    })
+  }
+
+  handleBoth = (e) => {
+    if (e.target.innerText === "+"){
+      this.setState({
+        number:this.state.number + 1
+      })
+    } 
+    else if (e.target.innerText === "-"){
+      this.setState({
+        number: this.state.number - 1
+      })
+    }
+  }
 
   render() {
     return (
       <div className="App">
         <h1>The current number is {this.state.number}</h1>
-        <h2>+</h2>
-        <h2>-</h2>
+        <h2 onClick={this.handleBoth}>+</h2>
+        <h2 onClick={this.handleBoth}>-</h2>
       </div>
     );
   }
+
 }
 
 export default App;
